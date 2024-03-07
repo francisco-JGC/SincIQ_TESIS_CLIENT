66c459eaeb5937cef55fffe146f0ce9041ca5797
Francisco Garcia <jjjchico1@gmail.com>
2024-03-07 01:08:42 -0600

    Add NavLink component to Navigation

---
+++ src/components/Navigation/navLink.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Link from 'next/link'
+import { useSelectedLayoutSegment } from 'next/navigation'
+
+interface NavLinkProps {
+  href: string
+  children: React.ReactNode
+  segment?: string
+}
+
+export default function NavLink({ href, children, segment }: NavLinkProps) {
+  let currentSegment = useSelectedLayoutSegment()
+
+  currentSegment === null && (currentSegment = 'dashboard')
+
+  const isActive = segment === currentSegment
+
+  return (
+    <Link href={href} className={'navlink ' + (isActive ? ' active' : '')}>
+      {children}
+    </Link>
+  )
+}
