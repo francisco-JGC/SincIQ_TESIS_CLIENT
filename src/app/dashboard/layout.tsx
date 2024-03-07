5fe013923b9d0b55cf9a8df9b2e41ec8e867c5c9
Francisco Garcia <jjjchico1@gmail.com>
2024-03-06 23:20:00 -0600

    Add dashboard layout component

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: '100',
+})
+
+export const metadata: Metadata = {
+  title: 'SincIQ',
+  description: 'SincIQ',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
