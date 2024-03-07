1a1e7718a4f3f165c2d89e95fbf1af965da1e229
Francisco Garcia <jjjchico1@gmail.com>
2024-03-07 01:08:32 -0600

    Add navigation icons and titles

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,3 +1,25 @@
+import Image from 'next/image'
 import './index.scss'
+import { NAVIGATION_ITEMS } from './items'
+import NavLink from './navLink'
 
-export const Navigation = () => {}
+export const Navigation = () => {
+  return (
+    <ul className="navigation__list">
+      {NAVIGATION_ITEMS.map((item, index) => (
+        <li key={index} className="navigation__item">
+          <NavLink href={item.href} segment={item.segment}>
+            <Image
+              src={item.icon}
+              alt={item.title}
+              width={24}
+              height={24}
+              typeof="svg"
+            />
+            <span>{item.title}</span>
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  )
+}
