80862fe4cdcada526da355552e76706c08d3787a
Francisco Garcia <jjjchico1@gmail.com>
2024-03-07 23:01:38 -0600

    Add CInput component

---
+++ src/components/CInput/index.tsx
@@ -0,0 +1,74 @@
+import './index.scss'
+import Image from 'next/image'
+
+interface CInputProps {
+  value: string
+  onChange: (e: any) => void
+  placeholder?: string
+  className?: string
+  type?: string
+  label?: string
+  label_span?: string
+  label_span_style?: React.CSSProperties
+  required?: boolean
+  name: string
+  id?: string
+  icon?: any
+  dissabled?: boolean
+  min?: number
+  max?: number
+}
+export const CInput = ({
+  value,
+  onChange,
+  placeholder,
+  className,
+  type,
+  label,
+  required,
+  name,
+  id,
+  icon,
+  dissabled,
+  min,
+  max,
+  label_span,
+  label_span_style,
+}: CInputProps) => {
+  return (
+    <div className={`c-input ${className}`}>
+      {label && (
+        <label className={`${required ? 'required' : ''}`} htmlFor={id}>
+          {label}{' '}
+          {label_span && (
+            <span
+              style={{
+                fontWeight: 'bold',
+                ...label_span_style,
+              }}
+            >
+              - {label_span}
+            </span>
+          )}
+        </label>
+      )}
+
+      <div className="c-input__container">
+        {icon && <Image className="icon" src={icon} alt="icon" />}
+
+        <input
+          id={id}
+          name={name}
+          type={type}
+          value={value}
+          onChange={(e) => onChange(e.target)}
+          placeholder={placeholder}
+          required={required}
+          disabled={dissabled}
+          min={min}
+          max={max}
+        />
+      </div>
+    </div>
+  )
+}
