b6988ee8a30e6256c412f70b5b51cacc72c1405a
Francisco Garcia <jjjchico1@gmail.com>
2024-03-08 00:13:58 -0600

    Update CSheet component props

--- src/components/CSheet/index.tsx
+++ src/components/CSheet/index.tsx
@@ -9,11 +9,12 @@ import {
 
 type Props = {
   children: React.ReactNode
-  title?: string
+  title: string
   description?: string
   Component?: React.ReactNode
   position?: 'left' | 'right' | 'top' | 'bottom'
   styles?: React.CSSProperties
+  className?: string
 }
 export function CSheet({
   children,
@@ -22,14 +23,24 @@ export function CSheet({
   position = 'right',
   Component,
   styles,
+  className,
 }: Props) {
   return (
     <Sheet>
-      <SheetTrigger asChild>{children}</SheetTrigger>
-      <SheetContent className="bg-white" side={position} style={styles}>
+      <SheetTrigger asChild className={className}>
+        {children}
+      </SheetTrigger>
+      <SheetContent
+        className="bg-[#100e17] border-none"
+        side={position}
+        style={{
+          boxShadow: '0px 4px 20px rgba(105, 46, 227, 0.2)',
+          ...styles,
+        }}
+      >
         <SheetHeader className="mb-4">
           <SheetTitle>{title}</SheetTitle>
-          <SheetDescription>{description}</SheetDescription>
+          {description && <SheetDescription>{description}</SheetDescription>}
         </SheetHeader>
         {Component}
       </SheetContent>
