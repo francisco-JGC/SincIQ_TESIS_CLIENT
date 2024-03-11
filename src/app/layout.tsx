c5a0953ae44a566baaecdd5640e9f7dbb494352c
Francisco Garcia <jjjchico1@gmail.com>
2024-03-10 22:28:24 -0600

    Update import statement for SessionProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.scss'
-import { Providers } from './providerSession'
+import { Providers } from './SessionProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
