b1e5b23f87ceab4227766c01c29e3d5892dc3124
Francisco Garcia <jjjchico1@gmail.com>
2024-03-10 22:28:28 -0600

    Add sign out functionality and display user information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,33 @@
 'use client'
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, useSession, signOut } from 'next-auth/react'
+import Image from 'next/image'
 
 export default function Home() {
   const { data: session } = useSession()
-
-  console.log(session)
 
   return (
     <nav>
       <h1>Sync IQ session</h1>
       <ul>
-        <li>
-          <button>Cerrar sesion</button>
-        </li>
-        <li>
-          <button onClick={() => signIn()}>Sign in with Google</button>
-        </li>
+        {session ? (
+          <li>
+            <span>
+              Signed in as {session.user?.email} <br />
+              <Image
+                src={session.user?.image as string}
+                alt={session.user?.name as string}
+                width={32}
+                height={32}
+                className="rounded-full h-8 w-8"
+              />
+            </span>
+            <button onClick={() => signOut()}>Sign out</button>
+          </li>
+        ) : (
+          <li>
+            <button onClick={() => signIn()}>Sign in</button>
+          </li>
+        )}
       </ul>
     </nav>
   )
