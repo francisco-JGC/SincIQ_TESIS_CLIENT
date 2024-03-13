cb3002588ecabd8867922b847ef321dcfc47818e
Francisco Garcia <jjjchico1@gmail.com>
2024-03-13 17:52:19 -0600

    Add client list to ChatRoom component

--- src/components/ChatRoom/index.tsx
+++ src/components/ChatRoom/index.tsx
@@ -1,13 +1,16 @@
 import './index.scss'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
+import icon_user from '@/assets/img/icon_user.png'
 import notificationsIcon from '@/assets/icons/notifications.svg'
 import { Switch } from '../Switch'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useClientsStore } from '@/store/messages/clientsStore'
 
 export default function ChatRoom() {
   const { data: session } = useSession()
   const [notifications, setNotifications] = useState<boolean>(true)
+  const clients = useClientsStore((state) => state.clients)
 
   return (
     <div className="chat-room-container">
@@ -41,7 +44,37 @@ export default function ChatRoom() {
           />
         </div>
 
-        <div className="chat-room__contacts__chats"></div>
+        <div className="chat-room__contacts__chats">
+          <h3>MENSAJES</h3>
+          <div className="chat-room__contacts__chats__chat">
+            {clients.length < 1 && (
+              <p className="mt-8 font-semibold text-center">
+                ¡ Parece que aun no tienes clientes !
+              </p>
+            )}
+            {clients.map((client, index) => (
+              <div
+                key={index}
+                className="chat-room__contacts__chats__chat__item"
+              >
+                <Image
+                  src={client.image_url || icon_user}
+                  alt="user"
+                  width={50}
+                  height={50}
+                />
+                <div>
+                  <h4>{client.username}</h4>
+                  <small>{client.lastMessage[0].content}</small>
+                </div>
+
+                <div className="chat-room__contacts__chats__chat__item__info">
+                  <small>{client.lastMessage[0].timestamp}</small>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </section>
       <section className="chat-room__messages"></section>
     </div>
