dda9e263029aa30a317246c698b805f07c558f0b
Francisco Garcia <jjjchico1@gmail.com>
2024-03-13 01:33:30 -0600

    Refactor sign-in functionality to use Link component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,11 @@
 'use client'
-import { signIn, useSession, signOut } from 'next-auth/react'
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
-  const { data: session } = useSession()
-
   return (
     <nav>
       <h1>Sync IQ session</h1>
-      <ul>
-        {session ? (
-          <li>
-            <span>
-              Signed in as {session.user?.email} <br />
-              <Image
-                src={session.user?.image as string}
-                alt={session.user?.name as string}
-                width={32}
-                height={32}
-                className="rounded-full h-8 w-8"
-              />
-            </span>
-            <button onClick={() => signOut()}>Sign out</button>
-          </li>
-        ) : (
-          <li>
-            <button
-              onClick={() =>
-                signIn('google', {
-                  callbackUrl: `${window.location.origin}/dashboard`,
-                })
-              }
-            >
-              Sign in
-            </button>
-          </li>
-        )}
-      </ul>
+      <Link href="/login">Ir a iniciar sesion</Link>
     </nav>
   )
 }
