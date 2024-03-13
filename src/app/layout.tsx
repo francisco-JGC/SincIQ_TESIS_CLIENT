a66382f4adbd6c1be7d3bc97db589925acf32062
Francisco Garcia <jjjchico1@gmail.com>
2024-03-13 10:13:53 -0600

    Update SessionProvider import and path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.scss'
-import { Providers } from './SessionProvider'
+import { Providers } from '../context/SessionProvider'
 import { Toaster } from 'sonner'
 
 const poppins = Poppins({
