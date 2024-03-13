353a5429e645770f3ac20658d98165db31190040
Francisco Garcia <jjjchico1@gmail.com>
2024-03-13 14:06:39 -0600

    Add styling and user information to ChatRoom component

--- src/components/ChatRoom/index.tsx
+++ src/components/ChatRoom/index.tsx
@@ -1,7 +1,27 @@
+import './index.scss'
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
 export default function ChatRoom() {
+  const { data: session } = useSession()
+
   return (
-    <div>
-      <h1>Chat Room</h1>
+    <div className="chat-room-container">
+      <section className="chat-room__contacts">
+        <header className="chat-room__contacts__header">
+          <Image
+            src={session?.user?.image as string}
+            alt="user"
+            width={50}
+            height={50}
+          />
+
+          <div>
+            <h3>{session?.user?.name}</h3>
+            <small>En línea</small>
+          </div>
+        </header>
+      </section>
+      <section className="chat-room__messages"></section>
     </div>
   )
 }
