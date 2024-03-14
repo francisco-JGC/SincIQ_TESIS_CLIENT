2ffc92d5dfd3c714f6d32979b41cbd76f453c643
Francisco Garcia <jjjchico1@gmail.com>
2024-03-14 00:27:34 -0600

    Add chat room functionality and dropdown menu

--- src/components/ChatRoom/index.tsx
+++ src/components/ChatRoom/index.tsx
@@ -2,15 +2,30 @@ import './index.scss'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import icon_user from '@/assets/img/icon_user.png'
+import arrowIcon from '@/assets/icons/arrow_right.svg'
 import notificationsIcon from '@/assets/icons/notifications.svg'
 import { Switch } from '../Switch'
 import { useEffect, useState } from 'react'
-import { useClientsStore } from '@/store/messages/clientsStore'
+import { Client, useClientsStore } from '@/store/messages/clientsStore'
+import { momentDate } from '@/utils/momentDate'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+import { MoreHorizontal } from 'lucide-react'
+import { ChatSelected } from './ChatSelected'
 
 export default function ChatRoom() {
   const { data: session } = useSession()
   const [notifications, setNotifications] = useState<boolean>(true)
   const clients = useClientsStore((state) => state.clients)
+
+  const [selectedClient, setSelectedClient] = useState<Client>()
 
   return (
     <div className="chat-room-container">
@@ -45,8 +60,8 @@ export default function ChatRoom() {
         </div>
 
         <div className="chat-room__contacts__chats">
-          <h3>MENSAJES</h3>
-          <div className="chat-room__contacts__chats__chat">
+          <h3>CHATS</h3>
+          <div className="chat-room__contacts__chats__clients">
             {clients.length < 1 && (
               <p className="mt-8 font-semibold text-center">
                 ¡ Parece que aun no tienes clientes !
@@ -56,6 +71,7 @@ export default function ChatRoom() {
               <div
                 key={index}
                 className="chat-room__contacts__chats__chat__item"
+                onClick={() => setSelectedClient(client)}
               >
                 <Image
                   src={client.image_url || icon_user}
@@ -63,20 +79,63 @@ export default function ChatRoom() {
                   width={50}
                   height={50}
                 />
-                <div>
-                  <h4>{client.username}</h4>
-                  <small>{client.lastMessage[0].content}</small>
-                </div>
-
                 <div className="chat-room__contacts__chats__chat__item__info">
-                  <small>{client.lastMessage[0].timestamp}</small>
+                  <div className="chat-room__contacts__chats__chat__item__info__header">
+                    <h4>{client.username} </h4>
+                    <span>{momentDate(client.lastMessage[0].timestamp)}</span>
+                  </div>
+                  <small>{client.lastMessage[0].content}</small>
                 </div>
               </div>
             ))}
           </div>
         </div>
       </section>
-      <section className="chat-room__messages"></section>
+      <section
+        className={`chat-room__messages ${selectedClient ? 'active' : ''}`}
+      >
+        <ChatSelected
+          setSelectedClient={setSelectedClient}
+          selectedClient={selectedClient}
+          actionChat={(client) => console.log('Action Chat', client)}
+          arrowIcon={arrowIcon}
+        />
+      </section>
     </div>
+  )
+}
+
+interface IActionsChat {
+  client: Client
+}
+
+export const ActionChat = ({ client }: IActionsChat) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0 rotate-90">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="bg-[#100e17] border border-[#3a3a3a] rounded-md p-2"
+        align="end"
+        style={{
+          backgroundColor: '#100e17',
+          padding: '1em',
+          borderRadius: '10PX',
+        }}
+      >
+        {/* <DropdownMenuLabel>Acciones</DropdownMenuLabel> */}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Actualizar perfil</DropdownMenuItem>
+        <DropdownMenuItem>Archivos compartidos</DropdownMenuItem>
+        <DropdownMenuItem>Silenciar notificaciones</DropdownMenuItem>
+        <DropdownMenuItem>Desactivar BOT</DropdownMenuItem>
+        <DropdownMenuItem>Vaciar chat</DropdownMenuItem>
+        <DropdownMenuSeparator />
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
