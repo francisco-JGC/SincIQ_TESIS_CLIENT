87c19bb0a001042b4e3901260d0ed5be0442cd7f
Francisco Garcia <jjjchico1@gmail.com>
2024-03-14 22:40:43 -0600

    Add interfaces for socket messages and clients, and implement a method to set a message to a client's conversation

--- src/store/messages/clientsStore.ts
+++ src/store/messages/clientsStore.ts
@@ -34,11 +34,29 @@ export interface LastMessage {
   conversation_id: number
 }
 
+export interface ISocketMessage {
+  client: IClient
+  message: string
+  from: string
+  type_message: string
+  message_by: string
+}
+
+export interface IClient {
+  id?: string
+  username?: string
+  phone_number: string
+}
+
 type ClientsStore = {
   clients: Client[]
   addClient: (client: Client) => void
   removeClient: (id: number) => void
   searchClient: (id: number) => boolean
+  setMessageToClientConversation: (
+    phone_number: string,
+    message: Message,
+  ) => void
 }
 
 export const useClientsStore = create<ClientsStore>((set, get) => ({
@@ -50,4 +68,13 @@ export const useClientsStore = create<ClientsStore>((set, get) => ({
       clients: state.clients.filter((client) => client.id !== id),
     })),
   searchClient: (id) => get().clients.some((client) => client.id === id),
+  setMessageToClientConversation: (phone_number, message) =>
+    set((state) => ({
+      clients: state.clients.map((client) => {
+        if (client.phone_number === phone_number) {
+          client.conversations[0].messages.push(message)
+        }
+        return client
+      }),
+    })),
 }))
