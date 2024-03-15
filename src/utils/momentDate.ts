1d997c5eb1bf0c88b045bb94953611a984f907b7
Francisco Garcia <jjjchico1@gmail.com>
2024-03-14 22:40:49 -0600

    Refactor momentDate function to use default fromNow() method

--- src/utils/momentDate.ts
+++ src/utils/momentDate.ts
@@ -4,6 +4,6 @@ import 'moment/locale/es'
 moment.locale('es')
 
 export const momentDate = (date: string) => {
-  const formattedDate = moment(date, 'YYYYMMDD').fromNow(true)
-  return `hace ${formattedDate}`
+  const formattedDate = moment(date).fromNow()
+  return `${formattedDate}`
 }
