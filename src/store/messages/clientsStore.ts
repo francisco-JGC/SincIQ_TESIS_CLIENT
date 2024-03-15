ee21c674207d1cb397373ae89df977e1a9e09348
Francisco Garcia <jjjchico1@gmail.com>
2024-03-15 02:26:05 -0600

    Add persistence to clientsStore using zustand/middleware

--- src/store/messages/clientsStore.ts
+++ src/store/messages/clientsStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 export interface Client {
   id: number
@@ -60,22 +61,67 @@ type ClientsStore = {
   ) => void
 }
 
-export const useClientsStore = create<ClientsStore>((set, get) => ({
-  clients: [],
-  addClient: (client) =>
-    set((state) => ({ clients: [...state.clients, client] })),
-  removeClient: (id) =>
-    set((state) => ({
-      clients: state.clients.filter((client) => client.id !== id),
-    })),
-  searchClient: (id) => get().clients.some((client) => client.id === id),
-  setMessageToClientConversation: (phone_number, message) =>
-    set((state) => ({
-      clients: state.clients.map((client) => {
-        if (client.phone_number === phone_number) {
-          client.conversations[0].messages.push(message)
-        }
-        return client
-      }),
-    })),
-}))
+const getInitialData = (): ClientsStore => {
+  const storedData = localStorage.getItem('clientsStore')
+
+  if (!storedData) {
+    return {
+      clients: [],
+      addClient: () => {},
+      removeClient: () => {},
+      searchClient: () => false,
+      setMessageToClientConversation: () => {},
+    }
+  }
+
+  return JSON.parse(storedData)
+}
+
+export const useClientsStore = create(
+  persist<ClientsStore>(
+    (set, get) => ({
+      clients: getInitialData().clients,
+      addClient: (client) =>
+        set((state) => ({ clients: [...state.clients, client] })),
+      removeClient: (id) =>
+        set((state) => ({
+          clients: state.clients.filter((client) => client.id !== id),
+        })),
+      searchClient: (id) => get().clients.some((client) => client.id === id),
+      setMessageToClientConversation: (phone_number, message) =>
+        set((state) => ({
+          clients: state.clients.map((client) => {
+            if (client.phone_number === phone_number) {
+              // verificar si el ultimo mensaje es igual al mensaje que se va a enviar y si es del mismo sender para no duplicar el mensaje
+              const lastMessage =
+                client.conversations[0].messages[
+                  client.conversations[0].messages.length - 1
+                ]
+              if (
+                lastMessage.content === message.content &&
+                lastMessage.sender === message.sender
+              ) {
+                return client
+              }
+              client.conversations[0].messages.push(message)
+              client.lastMessage = [
+                {
+                  id: message.id,
+                  content: message.content,
+                  timestamp: message.timestamp,
+                  sender: message.sender,
+                  receiver: message.receiver,
+                  conversation_id: client.conversations[0].id,
+                },
+              ]
+            }
+            return client
+          }),
+        })),
+    }),
+    {
+      name: 'clientsStore', // Nombre para identificar el almacenamiento
+      getStorage: () => localStorage, // Almacenamiento a utilizar (localStorage o sessionStorage)
+    },
+  ),
+)
