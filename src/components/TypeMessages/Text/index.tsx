22a51ca14ff321aa58da3bd711969c69a33fa8d3
Francisco Garcia <jjjchico1@gmail.com>
2024-03-14 22:40:39 -0600

    Add id prop to TextMessage component

--- src/components/TypeMessages/Text/index.tsx
+++ src/components/TypeMessages/Text/index.tsx
@@ -4,11 +4,16 @@ import { Message } from '@/store/messages/clientsStore'
 
 export interface TextMessageProps {
   message: Message
+  id: string
 }
 
-export const TextMessage = ({ message }: TextMessageProps) => {
+export const TextMessage = ({ message, id }: TextMessageProps) => {
   return (
-    <div className="container-text__message" data-sender={message.sender}>
+    <div
+      className="container-text__message"
+      data-sender={message.sender}
+      id={id}
+    >
       <div className="text__message__date" data-sender={message.sender}>
         <small>{momentDate(message.timestamp)}</small>
       </div>
