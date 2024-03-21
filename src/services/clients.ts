74302d874abb324da29480c0830b6e4bbf323feb
Francisco Garcia <jjjchico1@gmail.com>
2024-03-20 23:51:47 -0600

    Update getClients and add clearConversationFromClient functions

--- src/services/clients.ts
+++ src/services/clients.ts
@@ -1,5 +1,5 @@
 export const getClients = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clients`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clients/`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -22,6 +22,22 @@ export const changeBotStatus = async (
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ client_id, bot_status }),
+    },
+  )
+
+  const data = await response.json()
+  return data
+}
+
+export const clearConversationFromClient = async (client_id: number) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/clients/clear-conversation`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ client_id }),
     },
   )
 
