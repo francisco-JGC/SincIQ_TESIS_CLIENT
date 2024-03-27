7fe07da72487966eb2ea7239480e02f050c181b9
Francisco Garcia <jjjchico1@gmail.com>
2024-03-26 22:32:51 -0600

    Add RenderUploadImage component for image upload

---
+++ src/app/dashboard/create-product/components/RenderUploadImage.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image'
+import xIcon from '@/assets/icons/x.svg'
+import galeryIcon from '@/assets/icons/galery.svg'
+
+interface IRenderUploadImage {
+  image: File | null
+  setImage: React.Dispatch<React.SetStateAction<File | null>>
+  handleSelectImage: (ref: React.RefObject<HTMLInputElement>) => void
+  refImag: React.RefObject<HTMLInputElement>
+  handleImageChange: (
+    e: React.ChangeEvent<HTMLInputElement>,
+    setImage: React.Dispatch<React.SetStateAction<File | null>>,
+  ) => void
+}
+
+export const RenderUploadImage = ({
+  image,
+  setImage,
+  handleSelectImage,
+  refImag,
+  handleImageChange,
+}: IRenderUploadImage) => {
+  return (
+    <div
+      className="cursor-pointer w-full h-[300px] bg-gray-800 rounded flex justify-center items-center"
+      onClick={() => handleSelectImage(refImag)}
+      style={{
+        backgroundImage: image ? `url(${URL.createObjectURL(image)})` : '',
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        position: 'relative',
+      }}
+    >
+      {image && (
+        <div
+          className="absolute top-2 right-2 w-[30px] h-[30px] p-1 rounded-full cursor-pointer flex items-center justify-center bg-gray-300"
+          onClick={(e) => {
+            e.stopPropagation()
+            setImage(null)
+          }}
+        >
+          <Image src={xIcon} alt="galery" />
+        </div>
+      )}
+      <input
+        type="file"
+        ref={refImag}
+        onChange={(e) => handleImageChange(e, setImage)}
+        className="hidden"
+        accept=".jpg, .jpeg, .png"
+      />
+      {!image && <Image src={galeryIcon} alt="galery" />}
+    </div>
+  )
+}
