531515636759c2a9e5e61641c6bcd890b1b88c03
Francisco Garcia <jjjchico1@gmail.com>
2024-03-27 00:49:18 -0600

    Add Modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,83 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import Image from 'next/image'
+
+const SIZE_DIALOG = {
+  sm: 'sm:max-w-sm',
+  md: 'sm:max-w-md',
+  lg: 'sm:max-w-lg',
+  xl: 'sm:max-w-xl',
+  '2xl': 'sm:max-w-2xl',
+  '3xl': 'sm:max-w-3xl',
+}
+
+interface ModalProps {
+  nameButton?: string
+  title?: string
+  description?: string
+  Component?: React.FC
+  buttonStyle?: React.CSSProperties
+  size?: keyof typeof SIZE_DIALOG
+  className?: string
+  icon?: any
+  marginIcon?: string
+  children?: React.ReactNode
+  style?: React.CSSProperties
+}
+
+export const Modal = ({
+  nameButton,
+  title,
+  Component,
+  buttonStyle,
+  description,
+  size = '3xl',
+  className,
+  icon,
+  marginIcon,
+  children,
+  style,
+}: ModalProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger
+        className={`${className} ${icon && 'flex items-center'}`}
+        style={buttonStyle}
+      >
+        {icon && (
+          <Image
+            src={icon}
+            style={{ marginRight: marginIcon }}
+            alt="icon"
+            width={16}
+            height={16}
+          />
+        )}
+        {children || nameButton}
+      </DialogTrigger>
+      <DialogContent
+        style={{
+          backgroundColor: '#100e17',
+          border: 'none',
+          boxShadow: '0px 4px 20px rgba(105, 46, 227, 0.2)',
+          color: '#fff',
+          ...style,
+        }}
+        className={`${SIZE_DIALOG[size]}`}
+      >
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+
+        {Component && <Component />}
+      </DialogContent>
+    </Dialog>
+  )
+}
