e195b6ee427e1fc08a3adfdf2a1e0ce2621d1ac6
Francisco Garcia <jjjchico1@gmail.com>
2024-03-29 17:03:34 -0600

    Add createOrUpdateCatalogue function to catalogue service

--- src/services/catalogue.ts
+++ src/services/catalogue.ts
@@ -8,3 +8,16 @@ export const getCatalogue = async () => {
   const data = await response.json()
   return data
 }
+
+export const createOrUpdateCatalogue = async (data: any) => {
+  console.log(data)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/catalogue`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  const responseData = await response.json()
+  return responseData
+}
