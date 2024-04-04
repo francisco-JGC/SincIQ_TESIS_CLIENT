61ef65098f2f491306887ad47b95cd7436690235
Francisco Garcia <jjjchico1@gmail.com>
2024-04-03 19:59:07 -0600

    Refactor product list layout

--- src/app/dashboard/catalogue/components/productList.tsx
+++ src/app/dashboard/catalogue/components/productList.tsx
@@ -1,8 +1,9 @@
 import type { IProduct } from '@/store/products/products'
 import { ProductItem } from './productItem'
+
 export const ProductList = ({ products }: { products: IProduct[] }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div>
       {products.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
