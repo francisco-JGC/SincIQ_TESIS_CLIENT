a0565d17628c6745d12eeb5d81f4cc9b587dd9e9
Francisco Garcia <jjjchico1@gmail.com>
2024-04-06 11:55:28 -0600

    Add delete functionality to ProductItem component

--- src/app/dashboard/catalogue/components/productList.tsx
+++ src/app/dashboard/catalogue/components/productList.tsx
@@ -1,21 +1,28 @@
-import type { IProduct } from '@/store/products/products'
+import { useProductsStore, type IProduct } from '@/store/products/products'
 import { ProductItem } from './productItem'
+import { deleteProductById } from '@/services/product'
+import { toast } from 'sonner'
 
 export const ProductList = ({ products }: { products: IProduct[] }) => {
+  const removeProduct = useProductsStore((state) => state.removeProduct)
+
+  const handleRemoveProduct = async (id: number) => {
+    toast.loading('Eliminando producto...')
+
+    const response = await deleteProductById(id)
+
+    toast.dismiss()
+
+    if (response.success) {
+      toast.success('Has eliminado el producto')
+      removeProduct(id)
+    } else {
+      toast.error('Ha ocurrido un error al eliminar el producto')
+    }
+  }
+
   return (
     <div className="products-list-container">
-      {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
-      ))}
-      {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
-      ))}
-      {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
-      ))}
-      {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
-      ))}
       {products.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
