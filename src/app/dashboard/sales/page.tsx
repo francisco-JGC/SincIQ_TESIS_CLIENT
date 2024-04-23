58fef95bdb12760d3a4f68ce736cd7722825420b
Francisco Garcia <jjjchico1@gmail.com>
2024-04-23 00:24:46 -0600

    Add sales page to dashboard

---
+++ src/app/dashboard/sales/page.tsx
@@ -0,0 +1,12 @@
+import { LayoutPage } from '@/components/LayoutPage'
+import './index.scss'
+
+export default function SalesPage() {
+  return (
+    <LayoutPage title="">
+      <div className="sales-page">
+        <h1>Ventas</h1>
+      </div>
+    </LayoutPage>
+  )
+}
