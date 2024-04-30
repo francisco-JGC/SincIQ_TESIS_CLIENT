d68c2b198cfe05c2c3d91ae44d6b2f810289fe52
Francisco Garcia <jjjchico1@gmail.com>
2024-04-30 10:37:25 -0600

    Refactor ListProduct component in sales dashboard

--- src/app/dashboard/sales/component/listProduct.tsx
+++ src/app/dashboard/sales/component/listProduct.tsx
@@ -1,7 +1,9 @@
+import { CInput } from '@/components/CInput'
 import { getProducts } from '@/services/product'
 import type { IProduct } from '@/store/products/products'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import searchIcon from '@/assets/icons/search_icon.svg'
 
 interface IProps {
   handleAddProduct: (product: any) => void
@@ -9,6 +11,18 @@ interface IProps {
 
 export const ListProduct = ({ handleAddProduct }: IProps) => {
   const [products, setProducts] = useState<IProduct[]>([])
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (e: any) => setSearch(e.value)
+
+  const productFiltered = useMemo(() => {
+    return products.filter((product) => {
+      const name = product.name.toLowerCase()
+      const searchValue = search.toLowerCase()
+
+      return name.includes(searchValue)
+    })
+  }, [products, search])
 
   useEffect(() => {
     getProducts().then((response) => {
@@ -17,43 +31,57 @@ export const ListProduct = ({ handleAddProduct }: IProps) => {
   }, [])
 
   return (
-    <div className="list_register_product">
-      {products.map((product: IProduct) => (
-        <div
-          key={product.id}
-          className="flex
+    <>
+      <div className="search">
+        <CInput
+          type="text"
+          value={search}
+          placeholder="Buscar nombre de producto"
+          onChange={handleSearch}
+          icon={searchIcon}
+          name="search"
+          className="search__input"
+        />
+      </div>
+      <div className="list_register_product">
+        {productFiltered.map((product: IProduct) => (
+          <div
+            key={product.id}
+            className="flex
           p-4
           rounded-md
           shadow-md
           gap-4
         "
-        >
-          <div>
-            <Image
-              src={product.images_url[0]}
-              alt={product.name}
-              width={100}
-              height={100}
-              objectFit="contain"
-            />
-          </div>
-          <div>
-            <h3 className="text-lg font-bold">{product.name}</h3>
-            <p>
-              Cantidad: <strong>{product.quantity}</strong>
-            </p>
-            <p>
-              Precio: <strong>{product.price}</strong>
-            </p>
+          >
+            <div>
+              <Image
+                src={product.images_url[0]}
+                alt={product.name}
+                width={100}
+                height={100}
+                objectFit="contain"
+              />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">{product.name}</h3>
+              <p>
+                Cantidad: <strong>{product.quantity}</strong>
+              </p>
+              <p>
+                Precio: <strong>{product.price}</strong>
+              </p>
 
-            <button
-              onClick={() => handleAddProduct({ ...product, quantity: 1 })}
-            >
-              Agregar
-            </button>
+              <button
+                onClick={() => handleAddProduct({ ...product, quantity: 1 })}
+                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+              >
+                Agregar
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   )
 }
