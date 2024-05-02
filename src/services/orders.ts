0626fab6f798b74cb2323cf1f364df6f5a4c56f8
Francisco Garcia <jjjchico1@gmail.com>
2024-05-01 22:56:06 -0600

    Add getSaleOrders function to orders service

--- src/services/orders.ts
+++ src/services/orders.ts
@@ -10,3 +10,17 @@ export const createOrder = async (order: any) => {
   const data = await response.json()
   return data
 }
+
+export const getSaleOrders = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/order/sales`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+  const data = await response.json()
+  return data
+}
